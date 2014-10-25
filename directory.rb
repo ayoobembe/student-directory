285be850b98797d30b9754462397c7a64741502c
Ayo <ayo.obembe@gmail.com>
2014-10-25 18:01:52 +0100

    Placed student names in array, placed 'puts' in an iterator, eliminated student_count variable

--- directory.rb
+++ directory.rb
@@ -1,23 +1,30 @@
-#First we print the list of students
-student_count = 15
 
+#students is an array of strings containing student names
+students = [
+	"Abdul Hassan",
+	"Alan Bridger",
+	"Alexander Levin",
+	"Ana Nogal",
+	"Andrew Harrison",
+	"Andrew Hercules",
+	"Atilla Mirk",
+	"Claudia B",
+	"Colin Smif",
+	"Daniel Cotler",
+	"Ella Schofield",
+	"Elena Garone",
+	"Sandrine Zhang-Ferron",
+	"Tim Scully",
+	"Victoria Stepanova",
+	"Alex Blease",
+]
+
+#print out the student names
 puts "The students in my cohort at makers academy are: "
 puts "------------"
-puts "Abdul Hassan"
-puts "Alan Bridger"
-puts "Alexander Levin"
-puts "Ana Nogal"
-puts "Andrew Harrison"
-puts "Andrew Hercules"
-puts "Atilla Mirk"
-puts "Claudia B"
-puts "Colin Smif"
-puts "Daniel Cotler"
-puts "Ella Schofield"
-puts "Elena Garone"
-puts "Sandrine Zhang-Ferron"
-puts "Tim Scully"
-puts "Victoria Stepanova"
-puts "Alex Blease"
+students.each do |student|
+	puts student
+end
+
 #Then we print the number of students in the cohort
-puts "Overall we have #{student_count} great students!"
+puts "Overall we have #{students.length} great students!"
